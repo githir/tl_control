a2632c1297b2dd7bc42a7db9a097b7579e86d3d6
hokamoto <hokamoto@hhq.suzuki.co.jp>
2019-01-08 15:37:01 +0900

    add python/test.py examples

--- python/test.py
+++ python/test.py
@@ -4,19 +4,23 @@
 import rospy
 from std_msgs.msg import String
 
-seq = ['GREEN,5','YELLOW,1','RED,5']
+seq = ['GREEN,5','YELLOW,1','RED,5'] ##normal
+#seq = ['YELLOW,0.8','BLANK,0.8']     ##blink
+#seq = ['REDYELLOWGREENLEFTCENTERRIGHT,0.8','BLANK,0.8'] ##test:blink all
 
 def talker():
   pub = rospy.Publisher('tl_state', String, queue_size=10)
   rospy.init_node('tl_sender', anonymous=True)
-#  r = rospy.Rate(1) # 10hz
+  r = rospy.Rate(10) # 10hz
   while not rospy.is_shutdown():
     for state in seq:
       tlstate,delay = state.split(',')
       rospy.loginfo(state)
       pub.publish(tlstate)
-      rospy.sleep(int(delay))
-#  r.sleep()
+      rospy.sleep(float(delay))
+#      r.sleep()
+#      pub.publish(tlstate)
+#      rospy.sleep(float(delay)-(1./10))
 
 if __name__ == '__main__':
   try:
